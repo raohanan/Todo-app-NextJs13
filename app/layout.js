7c2cf67dd38f80357faf44beb6120f455b9eff05
raohanan <ad.hanan01@gmail.com>
2023-01-25 10:42:48 -0800

    Fetching-Api

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head />
+      <body>
+        <h1>Awesome todo app!</h1>
+        {children}</body>
+    </html>
+  )
+}
---
+++ app/page.js
@@ -0,0 +1,11 @@
+import AddNewTodo from "./Add-new-todo";
+import TodoList from "./Todo-list";
+
+export default function page() {
+  return (
+    <div>
+      <AddNewTodo />
+      <TodoList />
+    </div>
+  );
+}
